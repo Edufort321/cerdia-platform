7b3dada9678ba77776b0a0192801c670edd06e00
Edufort321 <eric.dufort@cerdia.ai>
2025-04-08 19:46:32 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,18 @@
+import './globals.css'
+import Header from '@/components/Header'
+
 export const metadata = {
   title: 'Investissement CERDIA',
-  description: 'L’intelligence au service de l’investissement',
+  description: 'L’intelligence au service de l’investissement.',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
-      <body className="bg-gray-100 text-gray-900">{children}</body>
+      <body className="bg-gray-100 text-gray-800">
+        <Header />
+        <main className="p-6 max-w-5xl mx-auto">{children}</main>
+      </body>
     </html>
   )
 }
